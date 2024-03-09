d7f8a6387b873ded1f79f583f3a7f6b436a01bff
Danya Samoylov <130479785+Zshdun@users.noreply.github.com>
2024-03-09 07:33:16 +0700

    Add files via upload

---
+++ lab3_1_Dimensional_Automation/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace lab3_1_Dimensional_Automation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Number = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Number
+            // 
+            this.Number.Location = new System.Drawing.Point(619, 80);
+            this.Number.Mask = "000";
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(101, 20);
+            this.Number.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(633, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Код (000-255)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox Number;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab3_1_Dimensional_Automation/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3_1_Dimensional_Automation
+{
+    public partial class Form1 : Form
+    {
+        int sizeCells = 10;
+        int[,] fieldColors;
+        int countRows = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        void Init()
+        {
+            ClientSize = new Size(750, 600);
+            MouseDown += Draw;
+            DoubleBuffered = true;
+            fieldColors = new int[(ClientSize.Width - 150) / sizeCells, ClientSize.Height / sizeCells];
+
+            
+        }
+        protected override void OnPaint(PaintEventArgs e) 
+        {
+            InitCells(e.Graphics);
+            InitGrid(e.Graphics);
+        }
+        
+        private void Draw(object sender, MouseEventArgs e) 
+        {
+            var thisX = e.X;
+            fieldColors[0, thisX / sizeCells] = fieldColors[0, thisX / sizeCells] == 1 ? 0 : 1;
+            Invalidate();
+        }
+
+        string Converter()
+        {
+            int number = int.Parse(Number.Text);
+            string binary = Convert.ToString(number, 2);
+            int l = binary.Length;
+
+            for (int i = 0; i < 8 - l; i++)
+                binary = 0 + binary;
+            return binary;
+        }
+        void Executor()
+        {
+            string rule = Converter();
+            countRows = ClientSize.Height / sizeCells;
+            for (int i = 1; i < countRows; i++) 
+            {
+                for (int j = 0; j < (ClientSize.Width - 150) / sizeCells; j++) 
+                {
+                    var l = j - 1;
+                    var r = j + 1;
+                    if (j==(ClientSize.Width-150)/sizeCells-1)
+                    {
+                        r = 0;
+                    }
+                    if (j==0)
+                    {
+                        l = (ClientSize.Width - 150) / sizeCells - 1;
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "111")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[0].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "110")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[1].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "101")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[2].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "100")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[3].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "011")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[4].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "010")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[5].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "001")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[6].ToString());
+                    }
+                    if (fieldColors[i - 1, l].ToString() + fieldColors[i - 1, j].ToString() + fieldColors[i - 1, r].ToString() == "000")
+                    {
+                        fieldColors[i, j] = int.Parse(rule[7].ToString());
+                    }
+                } 
+                    
+            }
+            Invalidate();
+        }
+        private void InitGrid(Graphics g)
+        {
+            for (int i = 0; i<= countRows; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(0, sizeCells * i), new Point(ClientSize.Width - 150, sizeCells*i));
+            }
+            for (int i = 0; i <= (ClientSize.Width - 150) / sizeCells; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(sizeCells * i,0), new Point(sizeCells * i,sizeCells*countRows));
+            }
+        }
+
+        private void InitCells(Graphics g) 
+        {
+            for (int i = 0; i < (ClientSize.Width - 150) / sizeCells; i++)
+            {
+                    for (int j = 0; j < ClientSize.Height/sizeCells; j++)
+                {
+                    g.DrawLine(new Pen(fieldColors[i, j] == 1 ? Color.Black : Color.White, sizeCells), sizeCells * j, sizeCells * i + sizeCells/2, sizeCells * j + sizeCells, sizeCells * i + sizeCells/2);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Executor();
+        }
+    }
+}
